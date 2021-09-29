c0993f04424dd206574a992020a054d8cad0c68f
Jaroslav-Melichar <jarabaltazar@seznam.cz>
2021-09-29 12:22:25 +0200

    Update bash.sh

---
+++ bash.sh
@@ -0,0 +1,19 @@
+# Vytvoření aresáře : 
+mkdir websites
+
+# Kontrola adresáře : 
+tree websites
+
+# Otevření adresáře :
+cd websites 
+
+# Vytvoření složky : 
+mkdir cz
+
+# Vchod do složky cz 
+cd cz
+
+# Vytvoření souboru
+touch index.html
+
+#
